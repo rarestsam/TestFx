e193e2f464af7302784db760e91293b89e6c3f98
rarestsam <103150271+rarestsam@users.noreply.github.com>
2022-10-15 20:47:09 -0500

    more changes

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,24 +2,33 @@ package application;
 	
 
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 
 
-public class Main extends Application {
+public class Main extends Application 
+{
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) 
+	{
 		try {
 			
 			Parent root = FXMLLoader.load(getClass().getResource("Login_Screen.fxml"));
-			Scene scene = new Scene(root,1920,1080);
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Scene scene = new Scene(root,1920,1030);
+			Image icon= new Image("food.png");
+			stage.getIcons().add(icon);
+			stage.setTitle("Restauraunt");
+			stage.setScene(scene);
+			stage.show();
 			
-		} catch(Exception e) {
+		} 
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 		}
 	}
--- src/application/SceneController.java
+++ src/application/SceneController.java
@@ -26,6 +26,75 @@ public class SceneController
 	 @FXML 
 	 private TextField txtField;
 	 
+	
+	 
+	 public void loginreroute(ActionEvent event) throws IOException
+	 {
+		 /*
+		 for(int i =0;i<j.length;i++)
+			{
+				String s = String.valueOf(i+1); 
+				j[i]= s;
+			//	System.out.print("it Worked");
+			}
+			
+		 for(int i=0; i<j.length; i++) 
+		 {
+		 */
+		 int value_of_txt; 
+		 String text = txtField.getText();
+		 value_of_txt = Integer.parseInt(text);
+		
+		 if( value_of_txt < 10) 
+		     {
+			  Parent root = FXMLLoader.load(getClass().getResource("Waitstaff_Screen.fxml"));
+			  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			  scene = new Scene(root);
+			  stage.setScene(scene);
+			  stage.show();
+		     }
+		 
+		 if(value_of_txt > 10 && value_of_txt < 20)
+		 {
+			  Parent root = FXMLLoader.load(getClass().getResource("Manager_Screen.fxml"));
+			  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			  scene = new Scene(root);
+			  stage.setScene(scene);
+			  stage.show();
+		 }
+	 }
+	 
+	 public void switchToSceneWaitstaff(ActionEvent event) throws IOException
+	 {
+		
+			  Parent root = FXMLLoader.load(getClass().getResource("Waitstaff_Screen.fxml"));
+			  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			  scene = new Scene(root);
+			  stage.setScene(scene);
+			  stage.show();
+		 
+		 
+	 }
+	 public void switchToManager() throws IOException
+	 
+	 {
+		 
+		  Parent root = FXMLLoader.load(getClass().getResource("Waitstaff_Screen.fxml"));
+		  scene = new Scene(root);
+		  stage.setScene(scene);
+		  stage.show();
+		 
+	 }
+	 public void switchToSceneWaitstaff_manager(ActionEvent event) throws IOException
+	 {		
+		 		
+			  Parent root = FXMLLoader.load(getClass().getResource("Waitstaff_Screen.fxml"));
+			  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			  scene = new Scene(root);
+			  stage.setScene(scene);
+			  stage.show();
+	 }
+	 
 	 public void switchToLogin(ActionEvent event) throws IOException 
 	 {
 	  root = FXMLLoader.load(getClass().getResource("Login_Screen.fxml"));
@@ -34,26 +103,6 @@ public class SceneController
 	  stage.setScene(scene);
 	  stage.show();
 	 }
-	 
-	 public void switchToSceneWaitstaff(ActionEvent event) throws IOException {
-		 
-		 for(int i =0;i<j.length;i++)
-			{
-				String s = String.valueOf(i+1); 
-				j[i]= s;
-			//	System.out.print("it Worked");
-			}
-		 for(int i=0; i<j.length; i++) 
-		 {
-		 if(txtField.getText().equals(j[i]) ) 
-		 {
-			  Parent root = FXMLLoader.load(getClass().getResource("Waitstaff_Screen.fxml"));
-			  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-			  scene = new Scene(root);
-			  stage.setScene(scene);
-			  stage.show();
-		 }
-		 }
-	 }
+	   
 }
 
