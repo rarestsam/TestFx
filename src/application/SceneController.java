07ae15f5ecf8898d1b297571edf88dc2e08eb97e
MayaS1111 <113066754+MayaS1111@users.noreply.github.com>
2022-10-29 21:30:13 -0500

    Waitstaff UI Update

--- src/application/SceneController.java
+++ src/application/SceneController.java
@@ -4,12 +4,24 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,10 +31,16 @@ import javafx.scene.Scene;
 
 public class SceneController
 {
+	 private static final Paint WHITE = null;
 	 private Stage stage;
 	 private Scene scene;
 	 private Parent root;
 	 String [] j = new String[10];
+	 private int fb = 1;
+	 private double ph = 200.0;
+	 //Pane foodbar = new AnchorPane();
+	 //Label foodText = new Label(null);
+	 //Label priceText = new Label(null);
 	 
 	// database
 	// setEditable(false) use this function for setting text in a textfield and not being played with
@@ -40,6 +58,33 @@ public class SceneController
 	 
 	 @FXML
 	 private Label login_Label;
+	 
+	 @FXML 
+	 private Pane foodBar;
+	 
+	 @FXML 
+	 private VBox foodBarBox;
+	 
+	 @FXML
+	 private Label totalPriceText;
+	 
+	 @FXML 
+	 private Button pizzaButton;
+	 
+	 @FXML 
+	 private Button burgerButton;
+	 
+	 @FXML 
+	 private Button icecreamButton;
+	 
+	 @FXML 
+	 private Button friesButton;
+	 
+	 @FXML 
+	 private Button sodaButton;
+	 
+	 @FXML 
+	 private Button waterButton;
 	 
 	 public void loginreroute(ActionEvent event) throws IOException
 	 {
@@ -140,6 +185,177 @@ public class SceneController
 		 	stage.setScene(scene);
 		 	stage.show();
 	 }
+	 
+	 public void addFoodBar(ActionEvent event)throws IOException
+	 {
+		 System.out.println("nothing");
+		 Font font21 = Font.font("Modern No. 20", 21);
+		 Font font29 = Font.font("Modern No. 20", 29);
+		 
+		 Pane foodbar = new AnchorPane();
+		 Label foodText = new Label(null);
+		 Label priceText = new Label(null);
+		 
+		 
+		 
+		 
+		 foodbar.setStyle("-fx-background-color: #ffffff; ");
+		 foodbar.setMinWidth(683);
+		 foodbar.setMinHeight(68);
+		 AnchorPane.setTopAnchor(foodbar, ph);
+		 //foodbar.setPadding(new Insets(0,0,0,0));
+		 foodbar.setId("foodbar"+fb);
+		 
+		 System.out.println("foodbar made");
+		 
+		 Spinner<Integer> spinner = new Spinner<> (1,1000,1);
+		 spinner.setMaxWidth(100);
+		 spinner.setMinHeight(49);
+		 spinner.relocate(40.0,10.0);
+		 spinner.setId("spinner"+fb);
+
+		 foodText.setMinWidth(179);
+		 foodText.setMinHeight(56);
+		 foodText.relocate(162.0,8.0);
+		 foodText.setFont(font29);
+		 foodText.setId("foodText"+fb);
+		 
+		 Label dollarSign = new Label("$");
+		 dollarSign.setMinWidth(16);
+		 dollarSign.setMinHeight(56);
+		 dollarSign.relocate(349.0,8.0);
+		 dollarSign.setFont(font29);
+		 dollarSign.setId("dollarSign"+fb);
+		 
+		 priceText.setMinWidth(103);
+		 priceText.setMinHeight(56);
+		 priceText.relocate(366.0,8.0);
+		 priceText.setFont(font29);
+		 priceText.setId("priceText"+fb);
+		 
+		 Button deleteButton= new Button("Delete");
+		 deleteButton.setMinWidth(90);
+		 deleteButton.setMinHeight(42);
+		 deleteButton.relocate(482.0,14.0);
+		 deleteButton.setStyle("-fx-background-color: #ff0000; ");
+		 deleteButton.setFont(font21);
+		 deleteButton.setId("deleteButton"+fb);
+		 
+		 Button noteButton= new Button("Notes");
+		 noteButton.setMinWidth(90);
+		 noteButton.setMinHeight(42);
+		 noteButton.relocate(584.0,14.0);
+		 noteButton.setStyle("-fx-background-color: #8e8e8e; ");
+		 noteButton.setFont(font21);
+		 noteButton.setId("noteButton"+fb);
+		 
+		 System.out.println("other stuff made");
+		 
+		 
+		 if(pizzaButton.isArmed()==true) 
+		 {
+			 foodText.setText("Pizza");
+			 priceText.setText("24.23");
+		 }
+		 else if(burgerButton.isArmed()==true) 
+		 {
+			 foodText.setText("Burger");
+			 priceText.setText("8.42");
+		 }
+		 else if(icecreamButton.isArmed()==true) 
+		 {
+			 foodText.setText("Ice Cream Scoop");
+			 priceText.setText("3.50");
+		 }
+		 else if(friesButton.isArmed()==true) 
+		 {
+			 foodText.setText("Fries");
+			 priceText.setText("3.02");
+		 }
+		 else if(sodaButton.isArmed()==true) 
+		 {
+			 foodText.setText("Soda");
+			 priceText.setText("4.30");
+		 }
+		 else if(waterButton.isArmed()==true) 
+		 {
+			 foodText.setText("Water Bottle");
+			 priceText.setText("2.54");
+		 }
+		 
+		 
+		 foodbar.getChildren().addAll(spinner,foodText,dollarSign,priceText,deleteButton,noteButton);
+		 System.out.println("added butt");
+		 
+		 foodBarBox.getChildren().addAll(foodbar);
+		 System.out.println(fb);
+		 
+		 ph += 100.0;
+		 fb ++;
+		 
+		 System.out.println(fb);
+		 
+		 /*
+		 root = FXMLLoader.load(getClass().getResource("Waitstaff_Screen.fxml"));
+		 stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		 Scene barScene = new Scene(foodBarBox);
+		 stage.setScene(barScene);
+		 stage.show();
+		 */
+		 
+	 }
+	 
+	 public void pizzaFoodBar(ActionEvent event)throws IOException
+	 {
+		 /*
+		 addFoodBar(event);
+		 foodText.setText("Pizza");
+		 priceText.setText("24.23");
+		 */
+	 }
+	 
+	 public void burgerFoodBar(ActionEvent event)throws IOException
+	 {
+		 /*
+		 addFoodBar(event);
+		 foodText.setText("Burger");
+		 priceText.setText("8.42");
+		 
+		 ph += 100.0;
+		 */
+	 }
+	 
+	 public void notesWindow(ActionEvent event)throws IOException{
+		 
+		 Stage note = new Stage();
+		 
+		 note.initModality(Modality.APPLICATION_MODAL);
+		 note.setTitle("Note");
+		 note.setMinWidth(400);
+		 note.setMinHeight(200);
+		 
+		 TextField noteText = new TextField();
+		 noteText.setText(null);
+		 noteText.setMinHeight(180);
+		 
+		 Button closeButton = new Button("Complete");
+		 closeButton.setOnAction(e -> note.close());
+		 
+		 VBox layout = new VBox(10);
+		 layout.getChildren().addAll(noteText,closeButton);
+		 
+		 Scene noteScene = new Scene(layout);
+		 note.setScene(noteScene);
+		 note.showAndWait();
+		 
+	 }
+	 
+	 public void deleteFoodBar(ActionEvent event)throws IOException{
+		
+		 
+		 
+	 }
+	 
 	 
 }
 
